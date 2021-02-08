f699424bb013ab3ca720fa5da043a811c46fd853
mnk665 <78741676+mnk665@users.noreply.github.com>
2021-02-08 22:34:44 +0400

    Add files via upload

---
+++ CSIT121 Assignment 1/src/Employees.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Employees {
+    private String name;
+    private Date dob;
+    private int ID;
+    private Date date_hired;
+    public enum gender{male,female};
+}
