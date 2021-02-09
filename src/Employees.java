baaaba8e840d57ed021082f0240c28d82e5abb80
mnk665 <mnk665@uowmail.edu.au>
2021-02-09 14:21:28 +0400

    Saving to GitHub

---
+++ src/Employees.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Employees {
+    private String name;
+    private Date dob;
+    private int ID;
+    private Date date_hired;
+    enum gender{male,female}
+}
---
+++ src/Managers.java
@@ -0,0 +1,5 @@
+public class Managers {
+    private double Salary;
+    private double budget;
+    private double pay_rise;
+}
